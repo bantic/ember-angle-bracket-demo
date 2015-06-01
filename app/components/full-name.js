32a3e18c12337ca9705112953a2cb2dd1bb4ba9d
Cory Forsyth <cory.forsyth@gmail.com>
2015-06-01 09:17:12 -0400

    Add a few examples of angle-bracket components

---
+++ app/components/full-name.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+const computed = Ember.computed;
+
+export default Ember.Component.extend({
+  firstName: computed.reads('attrs.first'),
+  lastName: computed.reads('attrs.last'),
+  fullName: computed('firstName', 'lastName', function(){
+    return this.get('firstName') + ' ' + this.get('lastName');
+  }),
+
+  actions: {
+    'do-something'() {
+      this.attrs['do-something']();
+    }
+  }
+});
